49a4b477455f8c7155f45beb5b14d132bf095789
Sergio <Sergio@DESKTOP-QS2QUEQ>
2019-03-16 14:29:19 +0100

    Initialize

---
+++ src/main/java/com/utad/curso/desarrollo/seguro/dto/DummyRequestBodyDto.java
@@ -0,0 +1,74 @@
+package com.utad.curso.desarrollo.seguro.dto;
+
+import java.util.List;
+
+import javax.validation.Valid;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+/**
+ * @author Sergio
+ *
+ */
+public class DummyRequestBodyDto {
+
+    private String attribute1;
+
+    @NotNull
+    @Min(0)
+    private Long attribute2;
+
+    @Valid
+    private DummyRequestSubAttribute attribute3;
+
+    private List<@NotBlank String> attribute4;
+
+    private List<@Valid DummyRequestSubAttribute> attribute5;
+
+    public List<DummyRequestSubAttribute> getAttribute5() {
+        return attribute5;
+    }
+
+    public void setAttribute5(
+            List<DummyRequestSubAttribute> attribute5) {
+        this.attribute5 = attribute5;
+    }
+
+    public DummyRequestSubAttribute getAttribute3() {
+        return attribute3;
+    }
+
+    public List<String> getAttribute4() {
+        return attribute4;
+    }
+
+    public void setAttribute4(
+            List<String> attribute4) {
+        this.attribute4 = attribute4;
+    }
+
+    public void setAttribute3(
+            DummyRequestSubAttribute attribute3) {
+        this.attribute3 = attribute3;
+    }
+
+    public String getAttribute1() {
+        return attribute1;
+    }
+
+    public void setAttribute1(
+            String attribute1) {
+        this.attribute1 = attribute1;
+    }
+
+    public Long getAttribute2() {
+        return attribute2;
+    }
+
+    public void setAttribute2(
+            Long attribute2) {
+        this.attribute2 = attribute2;
+    }
+
+}
---
+++ src/main/java/com/utad/curso/desarrollo/seguro/dto/DummyRequestSubAttribute.java
@@ -0,0 +1,26 @@
+package com.utad.curso.desarrollo.seguro.dto;
+
+public class DummyRequestSubAttribute {
+
+    private String subAttribute1;
+
+    private Long subAttribute2;
+
+    public String getSubAttribute1() {
+        return subAttribute1;
+    }
+
+    public void setSubAttribute1(
+            String subAttribute1) {
+        this.subAttribute1 = subAttribute1;
+    }
+
+    public Long getSubAttribute2() {
+        return subAttribute2;
+    }
+
+    public void setSubAttribute2(
+            Long subAttribute2) {
+        this.subAttribute2 = subAttribute2;
+    }
+}
